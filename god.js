7d8af642318ef0e36794d5dea695a711ed7a95cd
Hewsthon <44635673+Hewsthon@users.noreply.github.com>
2018-11-01 00:39:11 -0300

    Create god.js

---
+++ god.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const  client = new Discord.Client();
+client.on("ready", () => {
+   console.log(`Estoy listo`);
+
+   client.user.setPresence( {
+       status: "online",
+       game: {
+           name: `server.samphub.com`,
+           type: "PLAYING"
+       }
+   } );
+
+});
+client.on("Encendido", () => {
+   console.log("Estoy listo!");
+});
+client.on("message", (message) => {
+  if (message.content.startsWith("tarao")) {
+    message.channel.send("Tarao e 🖕 ");
+  } else
+  if (message.content.startsWith("hola")) {
+    message.channel.send("Hola soy el bot de Hewsthon, a tus ordenes");
+  } if (message.content.startsWith("robar")) {
+    message.channel.send("¿Alguien dijo robar?");
+  } else
+  if (message.content.startsWith("bot quien eres")) {
+     message.channel.send("Estoy hecho por un monton de codigos, preguntale a mi dios Hewsthon el me hizó de 0");
+  }
+
+  });
+client.login("NTA2OTQ5MzIyNzg1Njg1NTIy.DrwGbA.n6oCoWw0ueRm-Qe62WMIM2NJFW4");
